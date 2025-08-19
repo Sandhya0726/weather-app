5752d2650445ebb62a0093a6660c507747e24226
Sandhya0726 <neupanesandhya4567@gmail.com>
2025-08-19 17:39:30 +0545

    feat: add feature to get the weather of a city

---
+++ src/types/WeatherDataTypes.ts
@@ -0,0 +1,21 @@
+export type GeoData = {
+  name: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+};
+
+export type WeatherData = {
+  current_weather: {
+    temperature: number;
+    windspeed: number;
+    weathercode: number;
+  };
+  current_weather_units: {
+    temperature: string;
+    windspeed: string;
+  };
+  hourly: {
+    relative_humidity_2m: number[];
+  };
+};
