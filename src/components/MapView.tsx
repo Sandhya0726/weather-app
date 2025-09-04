a04ecc6463641c9ee58b5a4f2ea52ee26a72ef74
Sandhya0726 <neupanesandhya4567@gmail.com>
2025-09-04 12:48:30 +0545

    feat: add overlays in map

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -10,16 +10,35 @@ export function Map({ lat, lng, cityName }: MapProps) {
   return (
     <MapContainer
       center={[lat, lng]}
-      zoom={13}
+      zoom={7}
       style={{ height: '50vh', width: '100%', borderRadius: '8px' }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      />
+
+      <TileLayer
+        url={`https://tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png?appid=${
+          import.meta.env.VITE_OPENWEATHER_API_KEY
+        }`}
+        attribution="&copy; <a href='https://openweathermap.org/'>OpenWeatherMap</a>"
+      />
+
+      <TileLayer
+        url={`https://tile.openweathermap.org/map/temp/{z}/{x}/{y}.png?appid=${
+          import.meta.env.VITE_OPENWEATHER_API_KEY
+        }}`}
+      />
+
+      <TileLayer
+        url={`https://tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png?appid=${
+          import.meta.env.VITE_OPENWEATHER_API_KEY
+        }`}
       />
 
       <Marker position={[lat, lng]}>
-        <Popup>Hello,{cityName}</Popup>
+        <Popup>Hello, {cityName}</Popup>
       </Marker>
     </MapContainer>
   );
